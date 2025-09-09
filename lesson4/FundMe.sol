6a1f6add6d6f1cb85ce26c31a9383a114a52f57f
kirillspiney <131514855+kirillspiney@users.noreply.github.com>
2025-09-09 14:59:58 +1200

    Update FundMe.sol

--- lesson4/FundMe.sol
+++ lesson4/FundMe.sol
@@ -4,21 +4,29 @@ pragma solidity ^0.8.18;
 
 import {PriceConverter} from "./PriceConverter.sol";
 
+error NotOwner();
+
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 5e18;
+    uint256 public constant MINIMUM_USD = 5e18;
 
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
+    address public immutable i_owner;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
     
     function fund() public payable {
-        require(msg.value.getConversionRate() >= minimumUsd, "You need to spend more ETH!");
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withdraw() public {
+    function withdraw() public onlyOwner {
         for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
@@ -34,4 +42,18 @@ contract FundMe {
         require(callSuccess, "Call failed");
     }
     
+    modifier onlyOwner() {
+        // require(msg.sender == i_owner, "Sender is not owner");
+        if(msg.sender != i_owner) { revert NotOwner(); }
+        _;
+    }
+
+    // What happens if someone send this contract ETH without calling the fund function?
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
 }
